025aabb9513f751abcfe0535aea48468f1cf1850
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2023-07-19 12:38:56 +0200

    templates updated

--- src/templates/component.ts
+++ src/templates/component.ts
@@ -31,9 +31,13 @@ const initProps: {{it.name}}Props = {};
 const initProps = {};
 {{/if}}
 
-class {{it.name}} extends Component {
+class {{it.name}} extends Component{{@if(it.isTs)}}<{{it.name}}Props>{{/if}} {
 
-    constructor(props = initProps{{@if(it.isTs)}}: {{it.name}}Props{{/if}}) {
+    static defaultProps = {
+        ...initProps,
+    };
+
+    constructor(props{{@if(it.isTs)}}: {{it.name}}Props{{/if}} = initProps) {
         super(props);
     }
 
@@ -42,14 +46,13 @@ class {{it.name}} extends Component {
     }
 }
 
-{{it.name}}.defaultProps = initProps;
 export default {{it.name}};
 `;
 
 export const hoComponent = `
 import React from "react";
 
-const with{{it.name}} = (WrappedComponent{{@if(it.isTs)}}: React.FC | React.Component{{/if}}, ...args{{@if(it.isTs)}}: any[]{{/if}}) => {
+const {{it.name}} = (WrappedComponent{{@if(it.isTs)}}: React.FC | React.Component{{/if}}, ...args{{@if(it.isTs)}}: any[]{{/if}}) => {
     return (props{{@if(it.isTs)}}: any{{/if}}) => {
         return <WrappedComponent {...props} />
     }
@@ -62,15 +65,41 @@ export const hookComponent = `
 import React, { useState } from "react";
 
 {{@if(it.isTs)}}
-interface use{{it.name}}Values {}
+interface {{it.name}}Values {}
 {{/if}}
 
-const use{{it.name}} = (...args{{@if(it.isTs)}}: any[]{{/if}}){{@if(it.isTs)}}: use{{it.name}}Values{{/if}} => {
+const {{it.name}} = (...args{{@if(it.isTs)}}: any[]{{/if}}){{@if(it.isTs)}}: {{it.name}}Values{{/if}} => {
 
     const [sample, setSample] = useState{{@if(it.isTs)}}<any>{{/if}}();
 
     return {};
 }
 
-export default use{{it.name}};
+export default {{it.name}};
+`;
+
+export const lazyComponent = `
+import React, { lazy, Suspense } from "react";
+
+{{@if(it.isTs)}}
+interface {{it.name}}Props {}
+
+const initProps: {{it.name}}Props = {};
+{{#else}}
+const initProps = {};
+{{/if}}
+
+const {{it.name}}Lazy = lazy(() => import("./{{it.name}}"));
+
+const {{it.name}}{{@if(it.isTs)}}: React.FC<{{it.name}}Props>{{/if}} = (props = initProps) => {
+
+    const renderFallback = () => <>loading...</>;
+
+    return <Suspense fallback={renderFallback()}>
+        <{{it.name}}Lazy {...props} />
+    </Suspense>;
+}
+
+{{it.name}}.defaultProps = initProps;
+export default {{it.name}};
 `;
