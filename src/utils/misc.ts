c5a22c0ab34c55969c85a241956cf392b22a28e3
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2023-07-22 12:17:21 +0200

    added new util function

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,5 +1,14 @@
+import { readFile } from "fs/promises";
+import { join, normalize } from "path";
+import { cwd } from "process";
 import { PackageJSON } from "../interfaces/misc";
+
 
 export function getPackageJSON(): PackageJSON{
     return require('../../package.json');
+}
+
+export async function getFileContent(filepath: string) {
+    filepath = join(cwd(), normalize(filepath));
+    return await readFile(filepath, { encoding: "utf-8" });
 }
