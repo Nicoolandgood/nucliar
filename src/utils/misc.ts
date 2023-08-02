d6fe978d16baac1786c51d2e072c940296c79be5
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2023-08-02 21:41:10 +0200

    utils updated

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -8,11 +8,19 @@ export function getPackageJSON(): PackageJSON{
     return require('../../package.json');
 }
 
+export function getCwdFilepath(rawFilepath: string) {
+    return join(cwd(), normalize(rawFilepath));
+}
+
 export async function getFileContent(filepath: string) {
-    filepath = join(cwd(), normalize(filepath));
+    filepath = getCwdFilepath(filepath);
     return await readFile(filepath, { encoding: "utf-8" });
 }
 
 export function capitalize(input: string) {
     return input.charAt(0).toUpperCase() + input.slice(1);
+}
+
+export function getRandomToken() {
+    return Math.random().toString(36).slice(2);
 }
