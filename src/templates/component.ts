d639d378f91693cef5be8732af04ece624b116c0
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2023-08-03 00:32:55 +0200

    added template

--- src/templates/component.ts
+++ src/templates/component.ts
@@ -5,19 +5,19 @@ import './{{style.filename}}';
 {{/each}}
 
 {{@if(it.isTs)}}
-interface {{it.name}}Props {}
+interface {{it.computedName}}Props {}
 
-const initProps: {{it.name}}Props = {};
+const initProps: {{it.computedName}}Props = {};
 {{#else}}
 const initProps = {};
 {{/if}}
 
-const {{it.name}}{{@if(it.isTs)}}: React.FC<{{it.name}}Props>{{/if}} = (props = initProps) => {
+const {{it.computedName}}{{@if(it.isTs)}}: React.FC<{{it.computedName}}Props>{{/if}} = (props = initProps) => {
     return <></>; 
 }
 
-{{it.name}}.defaultProps = initProps;
-export default {{it.name}};
+{{it.computedName}}.defaultProps = initProps;
+export default {{it.computedName}};
 `;
 
 export const classComponent = `
@@ -27,20 +27,20 @@ import './{{style.filename}}';
 {{/each}}
 
 {{@if(it.isTs)}}
-interface {{it.name}}Props {}
+interface {{it.computedName}}Props {}
 
-const initProps: {{it.name}}Props = {};
+const initProps: {{it.computedName}}Props = {};
 {{#else}}
 const initProps = {};
 {{/if}}
 
-class {{it.name}} extends Component{{@if(it.isTs)}}<{{it.name}}Props>{{/if}} {
+class {{it.computedName}} extends Component{{@if(it.isTs)}}<{{it.computedName}}Props>{{/if}} {
 
     static defaultProps = {
         ...initProps,
     };
 
-    constructor(props{{@if(it.isTs)}}: {{it.name}}Props{{/if}} = initProps) {
+    constructor(props{{@if(it.isTs)}}: {{it.computedName}}Props{{/if}} = initProps) {
         super(props);
     }
 
@@ -49,60 +49,79 @@ class {{it.name}} extends Component{{@if(it.isTs)}}<{{it.name}}Props>{{/if}} {
     }
 }
 
-export default {{it.name}};
+export default {{it.computedName}};
 `;
 
 export const hoComponent = `
 import React from "react";
 
-const {{it.name}} = (WrappedComponent{{@if(it.isTs)}}: React.FC | React.Component{{/if}}, ...args{{@if(it.isTs)}}: any[]{{/if}}) => {
+const {{it.computedName}} = (WrappedComponent{{@if(it.isTs)}}: React.FC | React.Component{{/if}}, ...args{{@if(it.isTs)}}: any[]{{/if}}) => {
     return (props{{@if(it.isTs)}}: any{{/if}}) => {
         return <WrappedComponent {...props} />
     }
 }
 
-export default {{it.name}};
+export default {{it.computedName}};
 `;
 
 export const hookComponent = `
 import React, { useState } from "react";
 
 {{@if(it.isTs)}}
-interface {{it.name}}Values {}
+interface {{it.computedName}}Values {}
 {{/if}}
 
-const {{it.name}} = (...args{{@if(it.isTs)}}: any[]{{/if}}){{@if(it.isTs)}}: {{it.name}}Values{{/if}} => {
+const {{it.computedName}} = (...args{{@if(it.isTs)}}: any[]{{/if}}){{@if(it.isTs)}}: {{it.computedName}}Values{{/if}} => {
 
     const [sample, setSample] = useState{{@if(it.isTs)}}<any>{{/if}}();
 
     return {};
 }
 
-export default {{it.name}};
+export default {{it.computedName}};
 `;
 
 export const lazyComponent = `
 import React, { lazy, Suspense } from "react";
 
 {{@if(it.isTs)}}
-interface {{it.name}}Props {}
+interface {{it.computedName}}Props {}
 
-const initProps: {{it.name}}Props = {};
+const initProps: {{it.computedName}}Props = {};
 {{#else}}
 const initProps = {};
 {{/if}}
 
-const {{it.name}}Lazy = lazy(() => import("./{{it.name}}"));
+const {{it.computedName}}Lazy = lazy(() => import("./{{it.computedName}}"));
 
-const {{it.name}}{{@if(it.isTs)}}: React.FC<{{it.name}}Props>{{/if}} = (props = initProps) => {
+const {{it.computedName}}{{@if(it.isTs)}}: React.FC<{{it.computedName}}Props>{{/if}} = (props = initProps) => {
 
     const renderFallback = () => <>loading...</>;
 
     return <Suspense fallback={renderFallback()}>
-        <{{it.name}}Lazy {...props} />
+        <{{it.computedName}}Lazy {...props} />
     </Suspense>;
 }
 
-{{it.name}}.defaultProps = initProps;
-export default {{it.name}};
+{{it.computedName}}.defaultProps = initProps;
+export default {{it.computedName}};
+`;
+
+export const providerTemplate = `
+import React, { createContext, useContext } from "react";
+
+const initContextState = {};
+
+export const {{it.computedName}}Context = createContext(initContextState);
+
+export const use{{it.computedName}}Context = () => useContext({{it.computedName}}Context);
+
+const {{it.computedName}}Provider = (props) => {
+
+    return <{{it.computedName}}Context.Provider value={initContextState}>
+        {props.children}
+    </{{it.computedName}}Context.Provider>
+};
+
+export default {{it.computedName}}Provider;
 `;
