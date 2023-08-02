6380075573aa2240b1898a4eff892935c4e04589
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2023-08-02 21:42:29 +0200

    constants updated

--- src/constants/component.ts
+++ src/constants/component.ts
@@ -1,6 +1,17 @@
+import { classComponent, funcComponent, hoComponent, hookComponent } from "../templates/component";
+
 export enum ComponentType {
     DEFAULT = "component",
     CLASS = "class-component",
     HOC = "hoc",
     HOOK = "hook",
+}
+
+export const COMPONENT_TYPES = Object.values(ComponentType);
+
+export const COMPONENTS_TEMPLATES = {
+    [ComponentType.CLASS]: classComponent,
+    [ComponentType.DEFAULT]: funcComponent,
+    [ComponentType.HOC]: hoComponent,
+    [ComponentType.HOOK]: hookComponent,
 }
--- src/constants/generate.ts
+++ src/constants/generate.ts
@@ -1,16 +1,6 @@
-import { ClassComponent, FuncComponent, HoComponent, HookComponent } from "../core/component";
 import { GenerateOptions } from "../interfaces/commands";
-import { ComponentType } from "./component";
 import { StyleLanguage } from "./file";
 
-export const COMPONENTS_MAP = {
-    [ComponentType.DEFAULT] : FuncComponent,
-    [ComponentType.CLASS]: ClassComponent,
-    [ComponentType.HOC]: HoComponent,
-    [ComponentType.HOOK]: HookComponent,
-};
-
-export const COMPONENTS_MAP_KEYS = Object.keys(COMPONENTS_MAP);
 
 export const DEFAULT_OPTIONS: GenerateOptions = {
     dryRun: false,
