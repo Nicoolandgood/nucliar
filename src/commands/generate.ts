e11bc742671701e19efa89b9586e8036f1970846
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2023-08-02 21:42:59 +0200

    generate command updated

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,18 +1,25 @@
 import { createCommand } from "@commander-js/extra-typings";
 import { prompt } from 'enquirer';
-import { LazyComponent, DisplayComponent, CustomComponent } from "../core/component";
-import { ComponentType } from "../constants/component";
-import { STYLE_LANGAUGE_VALUES, StyleLanguage } from "../constants/file";
-import { Style } from "../core/style";
 import { join, normalize } from "path";
-import { GenerateOptions } from "../interfaces/commands";
-import { COMPONENTS_MAP, COMPONENTS_MAP_KEYS } from "../constants/generate";
-import { loadConfigurationFile } from "../utils/config";
 import { existsSync } from "fs";
+
+import { LazyComponent } from "../core/component";
+import { componentFactory } from "../core/factory";
+import { Style } from "../core/style";
+
+import { COMPONENT_TYPES, ComponentType } from "../constants/component";
+import { STYLE_LANGAUGE_VALUES, StyleLanguage } from "../constants/file";
+
+import { loadConfigurationFile } from "../utils/config";
 import { capitalize, getFileContent } from "../utils/misc";
-import { funcComponent } from "../templates/component";
 import { NucliarError } from "../utils/errors";
 import { getWarnText, logSuccess } from "../utils/logs";
+
+import { GenerateOptions } from "../interfaces/commands";
+
+import { funcComponent } from "../templates/component";
+
+
 
 async function handler(type: string, name: string, options: GenerateOptions) {
 
@@ -20,47 +27,39 @@ async function handler(type: string, name: string, options: GenerateOptions) {
     const typeOptions = types[type] ?? {};
     options = { ...configOptions, ...typeOptions, ...options };
 
-    const isVanilla = type in COMPONENTS_MAP;           // True if the type is included in nucliar.
+    const isVanilla = COMPONENT_TYPES.includes(type as unknown as ComponentType);  // True if the type is included in nucliar.
     const isCustom = (type in types) && !isVanilla;
 
-    let component;
+    if(!isVanilla && !isCustom) {
+        // Not valid type
+        const validTypes = new Set([...Object.keys(types), ...COMPONENT_TYPES]);
+        throw new NucliarError(`"${type}" is not a valid type. Possible types are: ${[...validTypes].join(', ')}.`);
+    }
+
+    const component = componentFactory(type, name, options.useTypescript, options.useJsx);
 
     if(isCustom) {
-        // Custom component type
+        // Custom type handling
         if(options.template && !existsSync(options.template)) {
             throw new NucliarError(`The template file ${options.template} does not exist.`);
         }
         const customTemplate = options.template? (await getFileContent(options.template)): funcComponent;
-        component = new CustomComponent(name, customTemplate, options.useTypescript, options.useJsx);
+        component.setTemplate(customTemplate);
     }
-    else if (!isVanilla) {
-        // Not valid type
-        const validTypes = Object.keys({ ...types, ...COMPONENTS_MAP }).join(', ');
-        throw new NucliarError(`"${type}" is not a valid type. Possible types are: ${validTypes}.`);
-    }
-    else {
-        // Vanilla component type
-        const _Component = COMPONENTS_MAP[type as ComponentType];
-        component = new _Component(name, options.useTypescript, options.useJsx);
-    }
-    
-    
-    if (component instanceof DisplayComponent) {
 
-        if(options.preprocessor && options.generateStyle) {
-            const preprocessor = (typeof options.preprocessor == "boolean"? StyleLanguage.CSS: options.preprocessor) as StyleLanguage;
+    if(options.preprocessor && options.generateStyle) {
+        const preprocessor = (typeof options.preprocessor == "boolean"? StyleLanguage.CSS: options.preprocessor) as StyleLanguage;
 
-            if(!STYLE_LANGAUGE_VALUES.includes(preprocessor))
-                throw new NucliarError(`"${preprocessor}" is not a valid style preprocessor. Possible values are: ${STYLE_LANGAUGE_VALUES.join(', ')}.`)
-            
-            const style = new Style(name, preprocessor as StyleLanguage, options.useCssModules);
-            component.setStyle(style);
-        }
-    
-        if(options.generateLazy) {
-            const lazy = new LazyComponent(name, options.useTypescript, options.useJsx);
-            component.setLazy(lazy);
-        }
+        if(!STYLE_LANGAUGE_VALUES.includes(preprocessor))
+            throw new NucliarError(`"${preprocessor}" is not a valid style preprocessor. Possible values are: ${STYLE_LANGAUGE_VALUES.join(', ')}.`)
+        
+        const style = new Style(name, preprocessor as StyleLanguage, options.useCssModules);
+        component.linkFiles(style);        
+    }
+
+    if(options.generateLazy) {
+        const lazy = new LazyComponent(name, options.useTypescript, options.useJsx);
+        component.linkFiles(lazy);
     }
 
     if (options.path) {
@@ -73,7 +72,7 @@ async function handler(type: string, name: string, options: GenerateOptions) {
     }
     
     if (options.dryRun) {
-        console.log(component.toString());
+        console.log(component.renderAll("\n\n"));
         return;
     }
 
@@ -90,14 +89,14 @@ async function handler(type: string, name: string, options: GenerateOptions) {
         }
     }
 
-    component.create();
-    logSuccess(`${capitalize(type)} generated at ${component.filepath}`);
+    component.createAll();
+    logSuccess(`${capitalize(type)} generated in ${component.filepath}`);
 }
 
 const cmd = createCommand("generate")
     .aliases(["gen", "g"])
     .description("Create a component element.")
-    .argument("<type>", `Component type (${COMPONENTS_MAP_KEYS.join(', ')})`)
+    .argument("<type>", "Component type.")
     .argument("<name>", "Component name.")
     .option("-d, --dry-run", "Prevent the file creation and output the render")
     .option("--use-typescript", "Writes the component in Typescript.")
