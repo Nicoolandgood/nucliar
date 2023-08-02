c61355e842c19f8d4dac799ee98853fbce74b6af
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2023-08-03 00:32:42 +0200

    factory function updated

--- src/core/factory.ts
+++ src/core/factory.ts
@@ -1,7 +1,25 @@
-import { COMPONENTS_TEMPLATES, ComponentType } from "../constants/component";
-import { Component } from "./component";
+import { ComponentType } from "../constants/component";
+import { ClassComponent, Component, FuncComponent, HoComponent, HookComponent, ProviderComponent } from "./component";
 
-export function componentFactory(componentType: ComponentType | string, name: string, isTs?: boolean, isJSX?: boolean, path?: string) {
-    const template = COMPONENTS_TEMPLATES[componentType as ComponentType];
-    return new Component(name, isTs, isJSX, template, path);
+export function componentFactory(componentType: ComponentType | string, name: string, isTs?: boolean, useJSX?: boolean, path?: string) {
+    
+    switch(componentType) {
+        case ComponentType.FUNCTIONAL:
+            return new FuncComponent(name, isTs, useJSX, path);
+        
+        case ComponentType.CLASS:
+            return new ClassComponent(name, isTs, useJSX, path);
+
+        case ComponentType.HOC:
+            return new HoComponent(name, isTs, useJSX, path);
+
+        case ComponentType.HOOK:
+            return new HookComponent(name, isTs, useJSX, path);
+
+        case ComponentType.PROVIDER:
+            return new ProviderComponent(name, isTs, useJSX, path);
+
+        default:
+            return new Component(name, isTs, useJSX, "", path);
+    }
 }
